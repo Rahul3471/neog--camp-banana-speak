8180f19e9e9f3440ab68349cd647f4a65ae78748
Rahul Rawat <113228214+Rahul3471@users.noreply.github.com>
2022-09-28 16:34:23 +0530

    taking input anf console

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
 
+console.log(txtInput)
+
+function clickHandler() {
+    console.log("clicked!");
+    console.log("input",txtInput.value);
+}
 btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log("clicked!")
+    
 })
